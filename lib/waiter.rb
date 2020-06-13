de1bd82e832f598b67e4ff1c6b9751cf524e72f5
kyle Dornan <kyledornan26@gmail.com>
2020-06-13 11:26:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,15 +1,3 @@
 class Waiter
-  attr_accessor :name, :age
- 
-  @@all = []
- 
-  def initialize(name, age)
-    @name = name
-    @age = age
-    @@all << self
-  end
- 
-  def self.all
-    @@all
-  end
+
 end
